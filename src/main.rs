4b0058e9076bbd2d5c30b1eb51a006dcc1870fa5
0xd34d10cc <0xd34d10cc@gmail.com>
2023-12-14 21:07:38 +0000

    Save & load command history

--- src/main.rs
+++ src/main.rs
@@ -9,25 +9,39 @@ mod query;
 use engine::Engine;
 use query::Query;
 
+const HISTORY_PATH: &str = "history.txt";
+
 fn main() -> Result<(), Box<dyn Error>> {
     let engine = Engine::new("camellia.db")?;
     let mut rl = DefaultEditor::new()?;
+    if let Err(e) = rl.load_history(HISTORY_PATH) {
+        println!("Failed to load history: {}", e);
+    }
+
     while let Ok(line) = rl.readline("> ") {
-        match Query::from_str(&line) {
+        let line = line.trim();
+        if line.is_empty() {
+            continue;
+        }
+
+        match Query::from_str(line) {
             Ok(query) => match engine.run(query) {
                 Ok(None) => {}
                 Ok(Some(rowset)) => {
                     println!("{}", rowset);
                 }
                 Err(e) => {
-                    eprintln!("Query failed: {}", e);
+                    println!("Query failed: {}", e);
                 }
             },
-            Err(e) => eprintln!("Failed to parse query: {}", e),
+            Err(e) => {
+                println!("Failed to parse query: {}", e)
+            }
         }
 
         rl.add_history_entry(line)?;
     }
 
+    rl.save_history(HISTORY_PATH)?;
     Ok(())
 }
